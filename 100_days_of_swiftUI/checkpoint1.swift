7e04820c628b91bf54fa082f066daddd4fc156b3
Eugenia <costamariaeugenia1@gmail.com>
2023-01-04 19:13:01 -0300

    adding checkpoint 1

---
+++ 100_days_of_swiftUI/checkpoint1.swift
@@ -0,0 +1,8 @@
+// Checkpoint 1
+// 1 - Creates a constant holding any temperature in Celsius.
+// 2 - Converts it to Fahrenheit by multiplying by 9, dividing by 5, then adding 32.
+// 3 - Prints the result for the user, showing both the Celsius and Fahrenheit value
+let celsius = 26.0
+let fahrenheit = (celsius*9)/5+32
+print("*** Checkpoint 1 ***")
+print("\(celsius) °C to Fahrenheit : \(fahrenheit) ° F")
