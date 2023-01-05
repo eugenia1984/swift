b4149339edd2b2e43290a79ff63bd79ed46a3e8d
Eugenia <costamariaeugenia1@gmail.com>
2023-01-05 18:39:59 -0300

    Create playground03_enum.swift

---
+++ 100_days_of_swiftUI/playground03_enum.swift
@@ -0,0 +1,14 @@
+import Cocoa
+// enums
+enum Weekday {
+    case Monday
+    case Tuesday
+    case Wednesday
+    case Thursday
+    case Friday
+}
+
+var day = Weekday.Monday
+print(day)
+day = Weekday.Tuesday
+print(day)
